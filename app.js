c0ed3bbc2c13af5a76dc2fc2d0b00d2fe84b1bf8
vikasp07 <155407261+vikasp07@users.noreply.github.com>
2024-01-31 13:35:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-
+const option = document.getElementById('search-select');
+option.addEventListener('change',function() {
+    var selectedFile=this.value;
+    if(selectedFile){
+        window.location.href=selectedFile;
+    }
+});
 
