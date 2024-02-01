cc291ee246c67b01a42f7621b931e2410228ff50
vikasp07 <155407261+vikasp07@users.noreply.github.com>
2024-02-01 15:41:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,34 @@ option.addEventListener('change',function() {
     }
 });
 
+const option = document.getElementById('search-select');
+const slider = document.getElementById('slider');
+const output = document.getElementById('value_output');
+const images = document.getElementById('image-container');
+
+const image=["image1.png","image2.png"];
+const initialImage=images[slider.value/(100/images.length)];
+images.innerHtml=`<img src="${initialImage}" alt="Slider Image">`;
+
+
+
+slider.addEventListner("input", () =>
+{
+    const currImageIndex=slider.value/(100/images.length);
+    const currImage=images[currImageIndex];
+    images.innerHtml=`<img src="${initialImage}" alt="Slider Image">`;
+
+
+    const value=slider.value;
+    output.textContent=value;
+});
+
+
+option.addEventListener('change',function() {
+    var selectedFile=this.value;
+    if(selectedFile){
+        window.location.href=selectedFile;
+    }
+});
+
+
